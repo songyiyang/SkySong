6a3b660f9b0faae2ff097f2f699cc1f374e5a793
Yiyang Song <songyiyang2012@gmail.com>
2014-03-23 14:01:02 -0400

    add some more seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email: "songyiyang@ufl.edu", password: "foobar55", password_confirmation: "foobar55")
+User.create(email: "songyiyang2012@gmail.com", password: "foobar55", password_confirmation: "foobar55")
+User.create(email: "song@gmail.com", password: "foobar55", password_confirmation: "foobar55")
+User.create(email: "song@126.com", password: "foobar55", password_confirmation: "foobar55")
