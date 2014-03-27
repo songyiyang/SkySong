706a1478e08f35758f43a564d5245cbf1b456ff5
Yiyang Song <songyiyang2012@gmail.com>
2014-03-27 15:59:53 -0400

    refactor

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -98,16 +98,7 @@ class PagesController < ApplicationController
   def send_img
     data_1 = params[:image_1]
     data_2 = params[:image_2]
-    image_data_1 = Base64.decode64(data_1['data:image/png;base64,'.length .. -1])
-    image_data_2 = Base64.decode64(data_2['data:image/png;base64,'.length .. -1])
-    File.open("#{Rails.root}/public/uploads/image_1.png", 'wb') do |f|
-      f.write image_data_1
-    end
-    File.open("#{Rails.root}/public/uploads/image_2.png", 'wb') do |f|
-      f.write image_data_2
-    end
-    @email = current_user.email
-    Notifier.send_image(@email).deliver
+    Channel.send_image(data_1, data_2, current_user)
     render json: {:msg => "Success!"}
   end
 
--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,2 +1,14 @@
 class Channel < ActiveRecord::Base
+  def self.send_image(data_1, data_2, current_user)
+    image_data_1 = Base64.decode64(data_1['data:image/png;base64,'.length .. -1])
+    image_data_2 = Base64.decode64(data_2['data:image/png;base64,'.length .. -1])
+    File.open("#{Rails.root}/public/uploads/image_1.png", 'wb') do |f|
+      f.write image_data_1
+    end
+    File.open("#{Rails.root}/public/uploads/image_2.png", 'wb') do |f|
+      f.write image_data_2
+    end
+    @email = current_user.email
+    Notifier.send_image(@email).deliver
+  end
 end
